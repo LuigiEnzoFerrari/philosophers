9e15e24d231b59df125dc6d8757c6549a07ce2b7
Luigi Ferrari <luigienzopereiraferrari@gmail.com>
2022-03-04 22:14:11 -0300

    create philosophers

--- main.c
+++ main.c
@@ -1,54 +1,25 @@
 #include "philo.h"
 
-void	print_dragon(void)
-{
-	for (int i = 0; i < 3; i++)
-	{
-		sleep(1);
-		printf("dragon\n");
-	}   
-}
-
-void	*print_dog(void *one)
-{
-	int *ptr;
-
-	ptr = malloc(sizeof(int));
-	*ptr = 4;
-	for (int i = 0; i < 7; i++)
-	{
-		sleep(1);
-		printf("print_dog %d\n", *ptr);
-		(*ptr)++;
-	}
-	return (void *)ptr;
-}
-
 void	*lenzao(void *arg)
 {
-	pthread_mutex_t	*mut;
-
-	mut = (pthread_mutex_t *)arg;
-	usleep(5 * 500000);
-	pthread_mutex_unlock(mut);
+	write(1, "ola\n", 4);
 	return NULL;
 }
 
 int	main(int argc, char **argv)
 {
-	// if (argc < 6 || argc > 7)
-		// return (EXIT_FAILURE);
 	pthread_mutex_t	mut;
-	pthread_t	tread[5];
-	int	*count;
+	pthread_t		*tread;
+	int n;
 
-	pthread_mutex_init(&mut, NULL);
-	pthread_create(&tread, NULL, lenzao, &mut);
-	pthread_mutex_lock(&mut);
-	pthread_mutex_lock(&mut);
-	pthread_create(&tread, NULL, print_dog, NULL);
-	print_dragon();
-	pthread_join(tread, (void *)&count);
-	printf("result: %d\n", *count);
+	n = atoi(*(argv + 1));
+	tread = malloc(sizeof(pthread_t) * n);
+	for (int i = 0; i <= n; i++) {
+		pthread_create(tread, NULL, lenzao, NULL);
+	}
+	for (int j = 0; j <= n; j++) {
+		pthread_join(*tread, NULL);
+	}
+	free(tread);
 	return (EXIT_SUCCESS);
 }
