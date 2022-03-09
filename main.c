133b368aeb10914d7d45461736d3d0c05fcefefe
Luigi Ferrari <luigienzopereiraferrari@gmail.com>
2022-03-09 14:39:36 -0300

    Create: table struct

--- main.c
+++ main.c
@@ -7,7 +7,8 @@ void	time_to_waist(uint64_t thinking_about_her)
 
 void	*routine(void *arg)
 {
-	t_seats *seat;
+    t_table *table;
+    t_seats *seat;
 	timeval last_eat, bpoint;
 	uint64_t waited;
 	char names[22][15] = {"luigi", "caio", "rafael",
@@ -18,39 +19,43 @@ void	*routine(void *arg)
 		"juliano", "gui"};
 	
 	memset(&last_eat, 0, sizeof(timeval));
-	seat = (t_seats *)arg;
+    table = (t_table *)arg;
+    seat = table->seats;
 	while (42)
 	{
 		// take a fork
 		pthread_mutex_lock(&seat->pfork);
 		gettimeofday(&bpoint, NULL);
-		if (*seat->status == 0) {
+		if (table->status == 0) {
 			pthread_mutex_unlock(&seat->pfork);
 			return (NULL);
 		}
-		printf(" %6lu \t%-15s has taken a fork\n", micro_to_milli(difference(seat->ctime, bpoint)), names[seat->id]);
+		printf(" %6lu \t%-15s has taken a fork\n", micro_to_milli(difference(table->the_time, bpoint)), names[seat->id]);
 
 		// take the second and starts eating
-		pthread_mutex_lock(&seat->right->pfork);
+        if (seat == seat->right)
+            usleep(table->act_time[DYING]);
+        else
+            pthread_mutex_lock(&seat->right->pfork);
 		gettimeofday(&bpoint, NULL);
 		// diference he waited to eat (last_eat - start_eat)
 		if (last_eat.tv_sec != 0)
 			waited = difference(last_eat, bpoint);
 		else
-			waited = difference(seat->ctime, bpoint);
-		if (*seat->status == 0) {
+			waited = difference(table->the_time, bpoint);
+		if (table->status == 0) {
 			pthread_mutex_unlock(&seat->pfork);
 			return (NULL);
 		}
-		if (waited > seat->act[DYING]) {
-			*seat->status = 0;
-			printf(" %6lu \t%-15s IS DIED\n", micro_to_milli(difference(seat->ctime, bpoint)), names[seat->id]);
+		if (waited > table->act_time[DYING]) {
+			table->status = 0;
+			printf(" %6lu \t%-15s IS DIED\n", micro_to_milli(difference(table->the_time, bpoint)), names[seat->id]);
 		}
 		else {
 		// eating
-			printf(" %6lu \t%-15s is eating\n", micro_to_milli(difference(seat->ctime, bpoint)), names[seat->id]);
+			printf(" %6lu \t%-15s is eating\n", micro_to_milli(difference(table->the_time, bpoint)), names[seat->id]);
 			seat->eat[EATEN]++;
-			usleep(seat->act[EATTT]);
+			usleep(table->act_time[EATTT]);
 			if (seat->eat[EATEN] == seat->eat[LIMIT]) {
 				pthread_mutex_unlock(&seat->pfork);
 				pthread_mutex_unlock(&seat->right->pfork);
@@ -63,17 +68,17 @@ void	*routine(void *arg)
 		pthread_mutex_unlock(&seat->right->pfork);
 		// starts sleeping
 		gettimeofday(&last_eat, NULL);
-		if (*seat->status == 0) {
+		if (table->status == 0) {
 			return (NULL);
 		}
-		printf(" %6lu \t%-15s is sleeping\n", micro_to_milli(difference(seat->ctime, last_eat)), names[seat->id]);
-		time_to_waist(seat->act[SLEEP]);
+		printf(" %6lu \t%-15s is sleeping\n", micro_to_milli(difference(table->the_time, last_eat)), names[seat->id]);
+		time_to_waist(table->act_time[SLEEP]);
 		// starts thinking
 		gettimeofday(&bpoint, NULL);
-		if (*seat->status == 0) {
+		if (table->status == 0) {
 			return (NULL);
 		}
-		printf(" %6lu \t%-15s is thiking\n", micro_to_milli(difference(seat->ctime, bpoint)), names[seat->id]);
+		printf(" %6lu \t%-15s is thiking\n", micro_to_milli(difference(table->the_time, bpoint)), names[seat->id]);
 	}
 	return (NULL);
 }
@@ -97,25 +102,28 @@ void	add_one_fork_each_philo(t_seats *seats)
 	}
 }
 
-void	philo_perfil(t_seats *seats, char **argv)
+void    dinner_rules(t_table *table, char **argv)
 {
-	int		id;
-	int		*status;
-	t_seats	*ptr;
 	timeval	ctime;
 
-	id = 0;
-	status = malloc(sizeof(int));
-	*status = 1;
-	ptr = seats;
 	gettimeofday(&ctime, NULL);
+	table->the_time = ctime;
+	table->status = 1;
+	table->n_seats = atoi(*argv);
+	table->act_time[DYING] = milli_to_micro(atoi(*(argv + 1)));
+	table->act_time[EATTT] = milli_to_micro(atoi(*(argv + 2)));
+	table->act_time[SLEEP] = milli_to_micro(atoi(*(argv + 3)));
+}
+
+void	philo_perfil(t_table *table, t_seats *seats, char **argv)
+{
+	int		id;
+	t_seats	*ptr;
+
+	id = 0;
+	ptr = seats;
 	while (42) {
 		ptr->id = id++;
-		ptr->status = status;
-		ptr->ctime = ctime;
-		ptr->act[DYING] = milli_to_micro(atoi(*(argv + 1)));
-		ptr->act[EATTT] = milli_to_micro(atoi(*(argv + 2)));
-		ptr->act[SLEEP] = milli_to_micro(atoi(*(argv + 3)));
 		if (*(argv + 4) != NULL)
 			ptr->eat[LIMIT] = atoi(*(argv + 4));
 		ptr = ptr->right;
@@ -124,16 +132,16 @@ void	philo_perfil(t_seats *seats, char **argv)
 	}
 }
 
-void    everyone_setting(t_seats *seats)
+void    everyone_setting(t_table *table)
 {
 	t_seats     *ptr;
 
-	ptr = seats;
+	ptr = table->seats;
 	while (42) {
 		usleep(5);
-		pthread_create(&seats->philo, NULL, &routine, (void *)seats);
-		seats = seats->right;
-		if (seats == ptr)
+		pthread_create(&table->seats->philo, NULL, &routine, (void *)table);
+		table->seats = table->seats->right;
+		if (table->seats == ptr)
 			break ;
 	}
 }
@@ -167,16 +175,17 @@ void	clean_the_dirty_dishes(t_seats *seats)
 
 int	main(int argc, char **argv)
 {
-	t_seats	*seats;
+	t_table	table;
 	if (argc > 6 || argc < 5) {
 		return(write(STDERR_FILENO, "misssing a argument\n", 20));
 	}
-	seats = NULL;
-	add_seats(&seats, atoi(*(argv + 1)));
-	add_one_fork_each_philo(seats);
-	philo_perfil(seats, argv + 1);;
-	everyone_setting(seats);
-	wait_the_last_one(seats);
-	clean_the_dirty_dishes(seats);
+	table.seats = NULL;
+	add_seats(&table.seats, atoi(*(argv + 1)));
+	add_one_fork_each_philo(table.seats);
+	philo_perfil(&table, table.seats, argv + 1);
+    dinner_rules(&table, argv + 1);
+	everyone_setting(&table);
+	wait_the_last_one(table.seats);
+	clean_the_dirty_dishes(table.seats);
 	return (EXIT_SUCCESS);
 }
