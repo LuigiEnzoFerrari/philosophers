b862bf40203d91cd28fe39bd1d4225a13eb87131
Luigi Ferrari <luigienzopereiraferrari@gmail.com>
2022-03-11 13:01:34 -0300

    remove count time of eating when the philo finish

--- srcs/the_party/take_a_nap.c
+++ srcs/the_party/take_a_nap.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   take_a_nap.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
+/*   By: lenzo-pe <lenzo-pe@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/10 01:33:22 by lenzo-pe          #+#    #+#             */
-/*   Updated: 2022/03/10 20:31:43 by coder            ###   ########.fr       */
+/*   Updated: 2022/03/11 02:43:26 by lenzo-pe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,21 +17,23 @@ static void	time_to_waist(uint64_t thinking_about_her)
 	usleep(thinking_about_her);
 }
 
-static int	nap_time(t_seats *seats, t_timeval *points)
+static int	nap_time(t_seats *seats)
 {
-	gettimeofday(&points[LAST_EAT], NULL);
+	t_timeval	time;
+
+	gettimeofday(&time, NULL);
 	if (*seats->status == 0)
 		return (1);
 	printf(SLEEPING,
 		micro_to_milli(difference(seats->rules->the_time,
-				points[LAST_EAT])), seats->id);
+				time)), seats->id);
 	time_to_waist(seats->rules->act[SLEEP]);
 	return (0);
 }
 
 int	sleeping_and_thinking(t_seats *seats, t_timeval *points)
 {
-	nap_time(seats, points);
+	nap_time(seats);
 	gettimeofday(&points[BREAK], NULL);
 	if (*seats->status == 0)
 		return (1);
